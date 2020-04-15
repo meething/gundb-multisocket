8a023531477c1b5585902c022c002a2f2e1e5167
Glitch (hello-express) <none>
2020-04-15 17:51:14 +0000

    👮🍇 Checkpoint ./server.js:13690106/274

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-var Gun = require('gun');
-var http = require('http');
-var server = http.createServer().listen(300);
-var wss_event = new WebSocket.Server({
-noServer: true
-});
+const url = require('url');
+const Gun = require('gun');
+const http = require('http');
+const WebSocket = require('ws');
+var server = http.createServer().listen(3000);
+
+var wss_event = new WebSocket.Server({ noServer: true});
+
 server.on('upgrade', function (request, socket, head) {
   var pathname = url.parse(request.url).pathname;
-  if (pathname === '/sim_world') {
+  if (pathname === '/gun1') {
+      wss_event.handleUpgrade(request, socket, head, function (ws) {
+          wss_event.emit('connection', ws);
+      });
+  } else if (pathname === '/gun2') {
       wss_event.handleUpgrade(request, socket, head, function (ws) {
           wss_event.emit('connection', ws);
       });
