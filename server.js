1dd531035cd29ee078d40d5610989c5b1ab83402
Glitch (hello-express) <none>
2020-04-15 18:14:15 +0000

    🍫🐞 Checkpoint ./server.js:13690106/434 ./package.json:13690106/1337

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ const http = require('http');
 const WebSocket = require('ws');
 var server = http.createServer().listen(3000);
 
+var gun1 = Gun({peers:[], ws: { noServer: true}});
+var gun2 = Gun({peers:[], ws: { noServer: true}});
+
+const QuickLRU = require('quick-lru');
+const lru = new QuickLRU({maxSize: 100});
+
 var wss_event = new WebSocket.Server({ noServer: true});
 
 server.on('upgrade', function (request, socket, head) {
   var pathname = url.parse(request.url).pathname;
+  var gun
+  if (lru.has(pathname)){
+    
+  } else {
+    var gun = Gun({peers:[], ws: { noServer: true}});
+    lru.set(pathname,gun);
+  }
+  
   if (pathname === '/gun1') {
-      wss_event.handleUpgrade(request, socket, head, function (ws) {
-          wss_event.emit('connection', ws);
+      gun1.handleUpgrade(request, socket, head, function (ws) {
+          gun1.emit('connection', ws);
       });
   } else if (pathname === '/gun2') {
-      wss_event.handleUpgrade(request, socket, head, function (ws) {
-          wss_event.emit('connection', ws);
+      gun2.handleUpgrade(request, socket, head, function (ws) {
+          gun2.emit('connection', ws);
       });
   } else {
       socket.destroy();
