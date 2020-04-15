f0b3bec7a5ac96febd0ab08f699d77175e2e758e
Glitch (hello-express) <none>
2020-04-15 15:57:12 +0000

    🙈🏵 Checkpoint ./server.js:13690106/68

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 var Gun = require('gun');
 var server = require('http').createServer().listen(3000);
-var gun = Gun({web: server});
+var gun1 = Gun({web: server, path: '/gun1'});
+var gun2 = Gun({web: server, path: '/gun2'});
