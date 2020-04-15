cf8f2cb180a3bcc6aa8f1a359a2240d673dc55cf
Glitch (hello-express) <none>
2020-04-15 19:58:13 +0000

    🏗🎃 Checkpoint ./server.js:13690106/227

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ var wss_event = new WebSocket.Server({ noServer: true});
 
 server.on('upgrade', async function (request, socket, head) {
   var pathname = url.parse(request.url).pathname || '/gun';
+  console.log('Got WS request',pathname);
   var gun = false;
   if (pathname){
       if (lru.has(pathname)){
         // Existing Node
+        console.log('Recycle id',pathname);
         gun = await lru.get(pathname);
       } else {
         // Create Node
+        console.log('Create id',pathname);
         gun = await Gun({peers:[], ws: { noServer: true}});
         lru.set(pathname,gun);
       }
