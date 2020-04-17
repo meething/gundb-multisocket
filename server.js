21d6a829718c69a0f2cee2c083bed7898b59cf9c
Glitch (hello-express) <none>
2020-04-17 09:21:22 +0000

    🐒📅 Checkpoint ./server.js:193094/105:13690106/720 ./memdisk.js:193094/5799

--- server.js
+++ server.js
@@ -8,19 +8,18 @@ const url = require('url');
 var rimraf = require("rimraf");
 const Gun = require('gun/gun');
 require('./gun-ws.js');
+require('./memdisk.js');
 const http = require('http');
 const WebSocket = require('ws');
 var server = http.createServer();
 
 // LRU with last used sockets
 const QuickLRU = require('quick-lru');
-const lru = new QuickLRU({maxSize: 10, onEviction: evict });
-
 var evict = function(key,value){
   console.log('Garbage Collect',key);
-  var uuid = value.gun.opt()._.opt.ws.uuid;
-  if(uuid) rimraf("/tmp/"+uuid, function () { console.log("Cleaned up ID",uuid); });
+  if(key) rimraf("/tmp/"+key, function () { console.log("Cleaned up ID",key); });
 }
+const lru = new QuickLRU({maxSize: 10, onEviction: evict });
 
 server.on('upgrade', async function (request, socket, head) {
   var pathname = url.parse(request.url).pathname || '/gun';
@@ -35,14 +34,12 @@ server.on('upgrade', async function (request, socket, head) {
         // Create Node
         console.log('Create id',pathname);
         // NOTE: Only works with lib/ws.js shim allowing a predefined WS as ws.web parameter in Gun constructor
-        var uuid = Math.random().toString(36).substring(7);
         gun.server = new WebSocket.Server({ noServer: true, path: pathname});
         console.log('set peer',request.headers.host+pathname);
         gun.gun = new Gun({ 
             peers:[], // should we use self as peer?
             localStorage: false,
-            uuid: uuid,
-            file: "tmp/"+uuid,
+            file: false,//"tmp/"+pathname,
             multicast: false,
             ws: { noServer: true, path: pathname, web: gun.server }, 
             web: gun.server 
